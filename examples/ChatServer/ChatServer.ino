ce8be7867dd0dc72e057268a5d8cc5f041eac9de
per1234 <per1234@users.noreply.github.com>
2015-10-05 00:42:53 -0700

    Clean up examples
    Fix typos and formatting

--- examples/ChatServer/ChatServer.ino
+++ examples/ChatServer/ChatServer.ino
@@ -1,20 +1,18 @@
 /*
- Chat  Server
+ Chat Server
 
  A simple server that distributes any incoming messages to all
- connected clients.  To use telnet to  your device's IP address and type.
+ connected clients.  To use, telnet to your device's IP address and type.
  You can see the client's input in the serial monitor as well.
- Using an Arduino Wiznet Ethernet shield or W5100 module.
+ Using an Arduino Wiznet Ethernet shield.
 
  Circuit:
- * Ethernet shield or W5100 module attached to pins 10, 11, 12, 13(uno/pro mini/nano/etc.) or pins 10, 51, 50, 52(mega)
+ * Ethernet shield attached to pins 10, 11, 12, 13
 
  created 18 Dec 2009
  by David A. Mellis
  modified 9 Apr 2012
  by Tom Igoe
- modified 2015-03-27
- by per1234
 
  */
 
@@ -44,7 +42,7 @@ void setup() {
   // Open serial communications and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
+    ; // wait for serial port to connect. Needed for native USB port only
   }
 
 
@@ -59,7 +57,7 @@ void loop() {
   // when the client sends the first byte, say hello:
   if (client) {
     if (!alreadyConnected) {
-      // clead out the input buffer:
+      // clear out the input buffer:
       client.flush();
       Serial.println("We have a new client");
       Serial.print("client IP address: ");
